d2359dac2b9ffcd9db5277df00391433f7c5f263
Harsh <Harshkoushal26@gmail.com>
2026-09-10 13:42:54 +0530

    add a function

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
-func main (){
-	
+
+import "fmt"
+
+func printNumb(Data int) {
+	if Data == 0 {
+		return
+	}
+	fmt.Println(Data)
+	printNumb(Data - 1)
+}
+
+func main() {
+	printNumb(5)
 }
